c35ba1e429a31fa8f94645e4dc5657abfd7696dd
[angela riggs] <[riggs.ang@gmail.com]>
2015-06-22 10:55:55 -0700

    added decrease counter button, and a button to clear value to 0

--- counter.js
+++ counter.js
@@ -6,28 +6,44 @@ var Counter = Backbone.Model.extend({
 var CounterView = Backbone.View.extend({
     render: function () {
         var val = this.model.get("value");
-        var btn = '<button>Increment</button>';
-        this.$el.html('<p>'+val+'</p>' + btn);
+        var btn = '<button id="btn">Increment</button>';
+        var btn2 = '<button id = "btn2">Decrease</button>';
+        var clr = '<button id = "clr">Clear</button>';
+        this.$el.html('<p>'+val+'</p>' + btn + btn2 + clr);
     }
 });
 
+var counterModel;
+var counterView;
 $(document).ready( function () {
 
-var counterModel = new Counter();
+    counterModel = new Counter();
 
-var counterView = new CounterView({model : counterModel});
-counterView.render();
-
-counterModel.on("change", function () {
+    counterView = new CounterView({model : counterModel});
     counterView.render();
-});
 
-counterView.$el.on("click","button", function () {
-    var mod = counterView.model;
-    var currVal = mod.get("value");
-    mod.set("value",currVal+1);
-});
+    counterModel.on("change", function () {
+        counterView.render();
+    });
 
-$("#counterdiv").append(counterView.$el);
+    counterView.$el.on("click","#btn", function () {
+        var mod = counterView.model;
+        var currVal = mod.get("value");
+        mod.set("value",currVal+1);
+    });
+
+    counterView.$el.on("click","#btn2", function () {
+        var mod = counterView.model;
+        var currVal = mod.get("value");
+        mod.set("value",currVal-1);
+    });
+
+    counterView.$el.on("click","#clr", function () {
+        var mod = counterView.model;
+        var currVal = mod.get("value");
+        mod.set("value",currVal = 0);
+    });
+
+    $("#counterdiv").append(counterView.$el);
 
 });
